7106779980f1da604939cc50dd89541060c24d3a
prayogateguh <teguh.p@students.amikom.ac.id>
2017-11-15 08:46:41 +0700

    Static pages

--- smk/urls.py
+++ smk/urls.py
@@ -21,22 +21,25 @@ from django.conf.urls.static import static
 # include admin app
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
 from berita.sitemaps import PostSitemap
 
 sitemaps = {
     'posts': PostSitemap,
 }
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # site authentication urls
     url(r'^accounts/', include('django.contrib.auth.urls')),
     # posts sitemaps
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    # static pages
+    url(r'^about/$', TemplateView.as_view(template_name='static/about_us.html'), name='about'),
+    url(r'^contact/$', TemplateView.as_view(template_name='static/contact_us.html'), name='contact'),
     # akun apps
     url(r'', include('akun.urls', namespace='akun', app_name='akun')),
-    # akun apps
+    # kbm apps
     url(r'', include('kbm.urls', namespace='kbm', app_name='kbm')),
     # berita apps
     url(r'', include('berita.urls', namespace='berita', app_name='berita')),
